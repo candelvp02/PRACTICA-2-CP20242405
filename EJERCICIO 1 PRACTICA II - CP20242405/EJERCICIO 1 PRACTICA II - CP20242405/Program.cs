1bf1770a4bdaed636af8f0c3f80c6cbba5bea63a
Candelaria Pereyra Villar <20242405@itla.edu.do>
2025-02-12 23:09:47 -0400

    Add files via upload

---
+++ EJERCICIO 1 PRACTICA II - CP20242405/EJERCICIO 1 PRACTICA II - CP20242405/Program.cs
@@ -0,0 +1,27 @@
+﻿class ejercicio1
+{
+    static void Main()
+    {
+        int numero;
+
+        while (true)
+        {
+            Console.WriteLine("Ingrese un numero de dos digitos: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero >= 10 && numero <= 99)
+            {
+                int decena = numero / 10;
+                int unidad = numero % 10;
+
+                Console.WriteLine($"El primer digito es: {decena}");
+                Console.WriteLine($"El segundo digito es: {unidad}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Intente de nuevo, por favor ingrese un numero de dos digitos (de 10 a 99).");
+            }
+        }
+    }
+}
---
+++ EJERCICIO 2 PRACTICA II - CP20242405/EJERCICIO 2 PRACTICA II - CP20242405/Program.cs
@@ -0,0 +1,27 @@
+﻿class ejercicio2
+{
+    static void Main()
+    {
+        int numero;
+
+        while (true)
+        {
+            Console.WriteLine("Ingrese un numero de dos digitos: ");
+            numero = int.Parse(Console.ReadLine());
+            
+            if (numero >= 10 && numero <= 99)
+            {
+                int decena = numero / 10;
+                int unidad = numero % 10;
+
+                int suma = decena + unidad;
+                Console.WriteLine($"La suma de ambos digitos es: {suma}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Intente de nuevo, por favor ingrese un numero de dos digitos (de 10 a 99).");
+            }
+        }
+    }
+}
